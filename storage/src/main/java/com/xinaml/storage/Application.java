ba66775dbcf2c46f6b5a019d02ba76dae8d99db1
lgq <xinaml@qq.com>
2019-05-05 18:29:32 +0800

    update

--- storage/src/main/java/com/xinaml/storage/Application.java
+++ storage/src/main/java/com/xinaml/storage/Application.java
@@ -2,12 +2,14 @@ package com.xinaml.storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class StorageApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(StorageApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
