cd316099003d16db253bfc4ef7ba15fc5c503dc7
lgq <xinaml@qq.com>
2019-05-05 18:23:45 +0800

    update

--- order/src/main/java/com/xinaml/order/Application.java
+++ order/src/main/java/com/xinaml/order/Application.java
@@ -1,13 +1,13 @@
-package com.xinaml.parent;
+package com.xinaml.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParentApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ParentApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- order/src/test/java/com/xinaml/order/OrderApplicationTests.java
+++ order/src/test/java/com/xinaml/order/OrderApplicationTests.java
@@ -1,4 +1,4 @@
-package com.xinaml.parent;
+package com.xinaml.order;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ParentApplicationTests {
+public class OrderApplicationTests {
 
     @Test
     public void contextLoads() {
