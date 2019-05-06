f52ba8a700aea64144e3401a0579673066df525a
lgq <xinaml@qq.com>
2019-05-06 09:28:04 +0800

    update

--- gateway/src/test/java/com/xinaml/gateway/ApplicationTests.java
+++ gateway/src/test/java/com/xinaml/gateway/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.xinaml.storage;
+package com.xinaml.gateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class StorageApplicationTests {
+public class ApplicationTests {
 
     @Test
     public void contextLoads() {
