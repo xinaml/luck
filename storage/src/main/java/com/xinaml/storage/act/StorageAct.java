dc285e7f17f6ace47a6d90f691349c9a025086a0
lgq <xinaml@qq.com>
2019-05-06 09:25:17 +0800

    update

---
+++ storage/src/main/java/com/xinaml/storage/act/StorageAct.java
@@ -0,0 +1,19 @@
+package com.xinaml.storage.act;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: [lgq]
+ * @Date: [19-5-6 上午8:56]
+ * @Description:
+ * @Version: [1.0.0]
+ * @Copy: [com.xinaml]
+ */
+@RestController
+public class StorageAct {
+    @GetMapping("test")
+    public String test() {
+        return "test";
+    }
+}
