efce79a0ac98df7db5b5b678014ee43fe73ec434
lgq <xinaml@qq.com>
2019-06-21 18:21:18 +0800

    update

--- modules/user/src/main/java/com/xinaml/user/ser/UserDetailsServiceImpl.java
+++ modules/user/src/main/java/com/xinaml/user/ser/UserDetailsServiceImpl.java
@@ -45,6 +45,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         boolean credentialsNonExpired = true; // 有效性 :true:凭证有效 false:凭证无效
         boolean accountNonLocked = true; // 锁定性 :true:未锁定 false:已锁定
         List<Role> roles =roleSer.findByUserId(user.getId());
+        roles.clear();
         for (Role role : roles) {
             //角色必须是ROLE_开头，可以在数据库中设置
             GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("ROLE_" + role.getName());
