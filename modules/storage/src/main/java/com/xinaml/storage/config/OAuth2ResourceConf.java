776b915e99411db5fdc0e15877b21435a8750b9b
lgq <xinaml@qq.com>
2019-06-28 18:14:42 +0800

    update

--- modules/storage/src/main/java/com/xinaml/storage/config/OAuth2ResourceConf.java
+++ modules/storage/src/main/java/com/xinaml/storage/config/OAuth2ResourceConf.java
@@ -1,5 +1,8 @@
 package com.xinaml.storage.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -8,6 +11,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+
+import java.net.URI;
 
 /**
  * @Author: [lgq]
@@ -19,11 +24,18 @@ import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 @Configuration
 @EnableResourceServer
 public class OAuth2ResourceConf extends ResourceServerConfigurerAdapter {
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
     @Primary
     @Bean
     public RemoteTokenServices tokenServices() {
+
+        ServiceInstance instance = loadBalancerClient.choose("user");
+        URI uri = instance.getUri();
+        // 令牌申请的地址
+        String authUrl = uri + "/oauth/check_token";
         final RemoteTokenServices tokenService = new RemoteTokenServices();
-        tokenService.setCheckTokenEndpointUrl("http://localhost:8083/oauth/check_token");
+        tokenService.setCheckTokenEndpointUrl(authUrl);
         tokenService.setClientId("webApp");
         tokenService.setClientSecret("123456");
         return tokenService;
